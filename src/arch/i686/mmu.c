5a65b6d605df51852dce6f4352019cefeb1a677b
Tori <toriahall7@gmail.com>
2025-06-28 17:55:03 +0100

    Ensured page tables are page-aligned

--- src/arch/i686/mmu.c
+++ src/arch/i686/mmu.c
@@ -2,7 +2,7 @@
 #include "../../../include/liballoc.h"
 
 static uint32_t page_directory[1024] __attribute__((aligned(4096)));
-static page_table_t* page_tables;
+static uint32_t page_table[1024] __attribute__((aligned(4096)));
 static uint32_t page_table_list_size = 1;
 static uint32_t page_table_list_cap  = 2;
 
@@ -16,11 +16,10 @@ uint8_t setupPageDirectory() {
         page_directory[i] = 0x2;
     }
 
-    page_tables = (page_table_t*)malloc(page_table_list_cap * sizeof(page_table_t));
     loadFirstTable();
 
     for(int i = 0; i < page_table_list_size; ++i) {
-        page_directory[i] = ((uint32_t)page_tables[i].data) | 3;
+        page_directory[i] = ((uint32_t)page_table) | 3;
     }
 
     loadPageDirectory((unsigned int*)page_directory);
@@ -30,6 +29,6 @@ uint8_t setupPageDirectory() {
 
 void loadFirstTable() {
     for(int i = 0; i < 1024; ++i) {
-        page_tables[0].data[i] = (i * 0x1000) | 3;
+        page_table[i] = (i * 0x1000) | 3;
     }
 }
